ac3028ea1d1a51c3fe1cc2f5785b9a5541b2fee4
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-09-15 20:59:50 +0530

    dao: contract
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- dao-contract/contracts/DAO.sol
+++ dao-contract/contracts/DAO.sol
@@ -1,8 +1,122 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^ 0.8.9;
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IERC20.sol";
+
+contract DAO is Ownable {
+
+    enum Side {
+        YES,
+        NO
+    }
+    enum Status {
+        UNDECIDED,
+        APPROVED,
+        REJECTED
+    }
+
+    struct Proposal {
+        address author;
+        bytes32 proposalnamehash;
+        uint256 createdAt;
+        uint256 votesYes;
+        uint256 votesNo;
+        Status status;
+    }
 
 
-contract DAO
-{
-    address public owner;
+    // proposalHashName to Proposal Struct data type
+    mapping(bytes32 => Proposal) public proposals;
+    
+    // who voter for who investor-> proposalHashName-> boolean
+    mapping(address =>  mapping(bytes32 => bool)) public votes;
+
+    // address of investor to amount
+    mapping(address => uint256 ) public shares;
+
+    uint256 public totalShares;
+
+    IERC20 public governanceToken;
+
+    // those who have 10 tokens minimum can create new proposals
+    uint256 constant CREATE_PROPOSAL_MIN_SHARE = 10 * (10**18);
+
+    uint256 constant VOTING_PERIOD = 7 days;
+
+    constructor(address _token)
+    {
+        governanceToken = IERC20(_token);
+    }
+
+
+    function deposit(uint256 amount) external
+    {
+        shares[msg.sender] += amount;
+        totalShares += amount;
+        governanceToken.transferFrom(msg.sender, address(this), amount);
+
+    }
+
+    function withdraw(uint256 amount) external
+    {
+        require(shares[msg.sender] >= amount, "not enough shares");
+
+        shares[msg.sender] -= amount;
+        
+        totalShares -= amount;
+
+        governanceToken.transfer(msg.sender, amount);
+    }
+
+    function createProposal(bytes32 proposalhash) external
+    {
+        require(shares[msg.sender] >= CREATE_PROPOSAL_MIN_SHARE , "not enough shares to create ");
+        require(proposals[proposalhash].proposalnamehash != bytes32(0),"Proposal Already Exits");
+
+        proposals[proposalhash] = Proposal(msg.sender, proposalhash, block.timestamp, 0, 0, Status.UNDECIDED  );
+
+    }
+
+
+    function vote(bytes32 proposalnamehash,Side side ) external
+    {
+        require(votes[msg.sender][proposalnamehash] == false, "Already Voted");
+        Proposal storage proposal = proposals[proposalnamehash];
+        require(proposal.proposalnamehash != bytes32(0), "Proposal Does not exists");
+        require(block.timestamp <= proposal.createdAt + VOTING_PERIOD , "Voting Period Over" );
+
+        votes[msg.sender][proposalnamehash] = true;
+
+
+        if(side == Side.YES)
+        {
+            proposal.votesYes +=   shares[msg.sender];          // the more governance token we have we can vote
+
+            // check end of votes i.e total share has been done
+
+            if(proposal.votesYes * 100 / totalShares  > 50)                 // votesYes/ totalShares > 0.5
+            {
+                proposal.status  = Status.APPROVED;
+            }
+
+
+        }
+        else
+        {
+            proposal.votesNo +=   shares[msg.sender];          // the more governance token we have we can vote
+
+            if(proposal.votesNo * 100 / totalShares  > 50)                 // votesYes/ totalShares > 0.5
+            {
+                proposal.status  = Status.REJECTED;
+            }
+
+        }
+
+
+
+
+    }
+
+
 }
--- dao-contract/contracts/VarmaToken.sol
+++ dao-contract/contracts/VarmaToken.sol
@@ -1,6 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract VarmaToken {
-    address public owner;
+
+contract VarmaToken is ERC20, Ownable {
+
+    uint256 constant ONE_TOKEN_DECIMAL = 10 **18;
+
+    constructor() ERC20("Varma's Token", "VARMA")
+    {
+        _mint(msg.sender, 100 * ONE_TOKEN_DECIMAL );
+    }
+
+    function mint(uint256 amount) external payable {
+
+        require(msg.value >= 0.01 ether , "Send Eth is too low.. It must greater than >= 0.01");
+
+        _mint(msg.sender, amount);
+    }
+
+    function withdraw() external onlyOwner() {
+
+        require(address(this).balance !=0 , "Balance is Low to send");
+
+        address payable WithdrawOwner = payable(owner());
+        WithdrawOwner.transfer(address(this).balance);
+
+    }
+    
+
 }
